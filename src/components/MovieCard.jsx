1c501b92fb817ef0e9b448a4eba6812c1f40cc05
maitri-dalvi <maitridalvi2004@gmail.com>
2025-05-11 20:20:15 +0530

    updated movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,28 @@ const MovieCard = ({movie: {title, vote_average, poster_path, release_date, orig
 }) => {
   return (
     <div className='movie-card'>
-        <p className='text-white'>{title}</p>
+        <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title} />
+        <div className="mt-4">
+            <h3>
+                {title}
+            </h3>
+            <div className="div">
+                <div className="div">
+                    <img src="star.svg" alt="Star Icon" />
+                    <p>{vote_average ? vote_average.toFixed(1): 'N/A'}</p>
+                </div>
+                <span>•</span>
+                <p className='lang'>
+                    {original_language}
+                </p>
+                <span>•</span>
+                <p className='year'>
+                    {release_date ? release_date.split('-')[0] : 'N/A'}
+                </p>
+
+            </div>
+
+        </div>
     </div>
   )
 }
